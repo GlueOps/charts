62ea043a1ac98b241067e09a23381494204eebec
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-11-04 22:18:50 +0000

    yolo

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-for D in `ls -d */`
-do
-    helm package --dependency-update $D
-done
+export AWS_REGION=us-west-2
 
-for F in `ls -f *.tgz`
+for group in `ls -d */`
 do
-    export AWS_REGION=us-west-2
-    helm s3 push $F glueops --ignore-if-exists
+    cd $group
+    helm s3 init s3://${{ secrets.S3_BUCKET_NAME }}/$group && helm repo add $group s3://${{ secrets.S3_BUCKET_NAME }}/$group
+
+    for D in `ls -d */`
+    do
+        helm package --dependency-update $D
+    done
+
+    for F in `ls -f *.tgz`
+    do
+        helm s3 push $F $group --ignore-if-exists
+    done
 done
