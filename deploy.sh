5c22b7b8c4357b2b0ed2b85c19588a69e4d0d8bb
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-11-04 22:26:14 +0000

    yolo

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ export AWS_REGION=us-west-2
 for group in `ls -d */`
 do
     cd $group
-    export chart_group=echo -n $group | tr -d "/"
+    export chart_group=`echo -n $group | tr -d "/"`
     helm s3 init s3://$S3_BUCKET_NAME/$chart_group && helm repo add $group s3://$S3_BUCKET_NAME/$chart_group
 
     for D in `ls -d */`
