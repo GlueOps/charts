2dfac11f5385f60079e95f4bd8645227c4eda9cd
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-11-04 22:52:01 +0000

    fixes

--- deploy.sh
+++ deploy.sh
@@ -3,28 +3,24 @@ set -e
 
 export AWS_REGION=us-west-2
 
-for group in `ls -d */`
-do
+for group in $(ls -d */); do
     cd $group
-    group=`echo -n $group | tr -d "/"`
+    group=$(echo -n $group | tr -d "/")
     helm s3 init s3://$S3_BUCKET_NAME/$group && helm repo add $group s3://$S3_BUCKET_NAME/$group
 
-    for D in `ls -d */`
-    do
+    for D in $(ls -d */); do
         helm package --dependency-update $D
     done
 
-    for F in `ls -f *.tgz`
-    do
+    for F in $(ls -f *.tgz); do
         if [ "$GLUEOPS_ENV" == "development" ]; then
-        helm s3 push --acl="public-read" --relative $F $group --force
+            helm s3 push --acl="public-read" --relative $F $group --force
         fi
 
         if [ "$GLUEOPS_ENV" == "production" ]; then
-        helm s3 push --acl="public-read" --relative $F $group --ignore-if-exists
+            helm s3 push --acl="public-read" --relative $F $group --ignore-if-exists
         fi
 
     done
     cd ..
 done
-
